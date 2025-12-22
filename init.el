bcadfd57cac482c0f22bb3962e71ab0c7a409fb7
j-chan-scb <jung.chan@sc.com>
2025-12-22 17:09:22 +0800

    refactor: removed theme, remove unecesary line

--- init.el
+++ init.el
@@ -20,10 +20,6 @@
 
 (add-hook 'org-mode-hook (lambda () (org-superstar-mode 1)))
 
-(load-theme 'modus-operandi :no-confirm)
-
-(set-face-attribute 'default nil :height 130)
-
 (custom-set-faces
   '(org-level-1 ((t (:inherit outline-1 :height 1.2))))
   '(org-level-2 ((t (:inherit outline-2 :height 1.15))))
