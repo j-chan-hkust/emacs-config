1a38e3c281b612b3fa6d77e19d8c84f16db69ada
j-chan-scb <jung.chan@sc.com>
2026-01-06 14:23:36 +0800

    feat: shift select enabled

--- init.el
+++ init.el
@@ -263,3 +263,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages nil))
+
+(setq org-support-shift-select t)
